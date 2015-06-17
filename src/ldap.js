52d2763fabd337385ded77fedce6febe68eb5b4f
Fan Wenbo <wbfan@thoughtworks.com>
2015-06-17 17:09:34 +0800

    security

--- src/ldap.js
+++ src/ldap.js
@@ -3,21 +3,22 @@ var ldapOptions = require('../res/ldap.json');
 
 function sync(ids, entryCb, cb) {
     var client = ldap.createClient({
-        "url": "ldap://chidc04.corporate.thoughtworks.com:389"
+        url: ldapOptions.url
     });
-    client.bind(ldapOptions.dn, ldapOptions.password, function(err) {
-        if(err) return console.log(err);
+    client.bind(ldapOptions.dn, ldapOptions.password, function (err) {
+        if (err) return console.log(err);
 
-        var treeBase = "ou=Employees,ou=Enterprise,ou=Principal,dc=corporate,dc=thoughtworks,dc=com";
         var filter = {
-            filter: '|' + ids.map(function(id) {return '(msSFU30UidNumber=' + id + ')'}).join(''),
+            filter: '|' + ids.map(function (id) {
+                return '(' + ldapOptions.idField + '=' + id + ')'
+            }).join(''),
             scope: 'sub'
         };
-        client.search(treeBase, filter, function(err, res) {
-            if(err) return console.log(err);
+        client.search(ldapOptions.treeBase, filter, function (err, res) {
+            if (err) return console.log(err);
 
             res.on('searchEntry', entryCb);
-            res.on('end', function() {
+            res.on('end', function () {
                 client.unbind();
                 cb();
             });
